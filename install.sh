ef7c95dd50a6bbddb168acd9c12e21e4fbd98027
nk2ge5k <nk2e5k@yandex-team.ru>
2019-09-12 01:08:32 +0300

    added .gitconfig
    updated install script

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+
 ln -sf $(pwd)/.vimrc $HOME/.vimrc
 ln -sf $(pwd)/.vimrc.plugins $HOME/.vimrc.plugins
 ln -sf $(pwd)/.zshrc $HOME/.zshrc
+
+cat >> $HOME/.gitconfig <<EOF
+[include]
+    $(pwd)/.gitconfig
+EOF
