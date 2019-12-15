8506a70749ddc40ba9a091abaa5330d551773402
nk2ge5k <nk2e5k@yandex-team.ru>
2019-12-15 19:01:18 +0300

    Reorganization

--- install.sh
+++ install.sh
@@ -1,12 +1,82 @@
 #!/bin/bash
 
-git clone https://github.com/VundleVim/Vundle.vim.git $HOME/.vim/bundle/Vundle.vim
+GO_VERSION=go1.13.5
+GO_ARCH=amd64
+GO_OS=linux
+if [[ "$(uname)" == "Darwin" ]]; then
+    GO_OS=darwin
+fi
 
-ln -sf $(pwd)/.vimrc $HOME/.vimrc
-ln -sf $(pwd)/.vimrc.plugins $HOME/.vimrc.plugins
-ln -sf $(pwd)/.zshrc $HOME/.zshrc
+WORK_DIR=$(pwd)
 
+# VIM
+ln -sf $WORK_DIR/editor/.vimrc $HOME/.vimrc
+ln -sf $WORK_DIR/editor/.vimrc.plugins $HOME/.vimrc.plugins
+# PATHOGEN
+mkdir -p ~/.vim/autoload ~/.vim/bundle && \
+    curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# TMUX 
+ln -sf $WORK_DIR/shell/tmux.conf $HOME/.tmux.conf
+
+# GIT
 cat >> $HOME/.gitconfig <<EOF
 [include]
-    $(pwd)/.gitconfig
+    $WORK_DIR/shell/.gitconfig
 EOF
+
+# FISH
+mkdir -p $HOME/.config &&
+    ln -sf $WORK_DIR/shell/fish/config.fish $HOME/.config/fish/config.fish
+
+executable() {
+    local program="${1}"
+
+    command -v $program > /dev/null
+    if [[ "${?}" -ne 0 ]]; then
+        return 1
+    fi
+
+    return 0
+}
+
+install() {
+    local program="${1}"
+
+    # FIXME
+    if [[ "$(uname)" == "Darwin" ]]; then
+        if ! executable $program; then
+            echo "Installing $program..."
+            #TODO(nk2ge5k): support other OS
+            brew install $program
+        else
+            echo "$program already installed"
+        fi
+    fi
+}
+
+# Install GO
+if ! executable go; then
+    curl --silent https://storage.googleapis.com/golang/$GO_VERSION.$GO_OS-$GO_ARCH.tar.gz | tar xvz -C $HOME
+fi
+
+# Install Rust
+if ! executable rust; then
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+fi
+
+# Install brew for MacOS
+if [[ "$(uname)" == "Darwin" ]]; then
+    if ! executable brew; then
+        echo "Installing brew..."
+        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    fi
+fi
+
+install fish
+install git
+install ssh
+install tmux
+install wget
+install mutt
+install w3c
---
+++ shell/fish/config.fish
@@ -0,0 +1,53 @@
+function mkdirc
+    mkdir -p $argv[1] && cd $argv[1]
+end
+
+function docker-tty
+    screen ~/Library/Containers/com.docker.docker/Data/vms/0/tty
+end
+
+set -U EDITOR vim
+set -x LANG "en_US.UTF-8"
+set -x LC_ALL "en_US.UTF-8"
+set -x LC_CTYPE "en_US.UTF-8"
+
+set -x GOPATH $HOME
+set -x GOROOT $HOME/go
+
+set -ax PATH $GOROOT/bin
+set -ax PATH $GOPATH/bin
+set -ax PATH $GOPATH/src/github.com/uber/go-torch/FlameGraph
+set -ax PATH /opt/local/bin
+set -ax PATH /opt/local/sbin
+set -ax PATH /usr/local/sbin
+set -ax PATH $HOME/.cargo/bin
+set -ax PATH /usr/local/opt/libpq/bin
+set -ax PATH /Users/nk2ge5k/yandex-cloud/bin
+
+function fish_prompt
+	set_color brblack
+	echo -n "["(date "+%H:%M")"] "
+	set_color yellow
+	echo -n (whoami)
+	set_color blue
+	if [ $PWD != $HOME ]
+		set_color brblack
+		echo -n ':'
+		set_color yellow
+		echo -n (basename $PWD)
+	end
+	set_color green
+	printf '%s ' (__fish_git_prompt)
+	set_color red
+	echo -n '> '
+	set_color normal
+end
+
+function fish_greeting
+	echo -e (uname -rs | awk '{print "\\\\e[1mOS: \\\\e[0;32m"$0"\\\\e[0m"}')
+	echo -e (uptime | sed 's/^up //' | awk '{print "\\\\e[1mUptime: \\\\e[0;32m"$0"\\\\e[0m"}')
+	echo -e (uname -n | awk '{print "\\\\e[1mHostname: \\\\e[0;32m"$0"\\\\e[0m"}')
+	echo -e "\n"
+
+	set_color normal
+end
